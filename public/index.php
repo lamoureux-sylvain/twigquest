94dd47134aa255159b944e4ad42624cee8ba0581
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2019-04-15 13:42:30 +0200

    envoie quete

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+require '../vendor/autoload.php';
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+$loader = new Twig\Loader\FilesystemLoader('../src/View');
+
+$twig = new Twig\Environment($loader);
+
+
+echo $twig->render('index.html.twig', ['products'=>$products, 'title'=>'les bon produits aiiiiiiggggghhtt']);
